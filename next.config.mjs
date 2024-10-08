a3ee6a59e4808a0f16601b09719030427bb838c3
Musab-Salah <musabsalah1998@gmail.com>
2024-10-08 17:07:07 +0300

    2.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Enables static exports
-  basePath: "/BigBangLive", // Correct base path (repository name)
-  reactStrictMode: true,
-  trailingSlash: true, // Ensures that all paths include a trailing slash for GitHub Pages
-};
+const nextConfig = {};
 
 export default nextConfig;
